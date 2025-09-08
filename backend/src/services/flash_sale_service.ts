5cd1b2fc520aff0a437be85d8a418102c95472d0
Kevin Caballas <kevincaballas@gmail.com>
2025-09-08 18:10:53 +0800

    Remove validation for unallowed request payload params

--- backend/src/services/flash_sale_service.ts
+++ backend/src/services/flash_sale_service.ts
@@ -1,13 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { FlashSaleRepository } from '../repositories/flash_sale_repository';
-import CreateFlashSalePayload from 'src/payloads/flash_sale/create_flash_sale_payload';
-
-interface FlashSaleServiceInterface {
-  create: (payload: CreateFlashSalePayload) => any;
-}
+import CreateFlashSalePayload from '../payloads/flash_sale/create_flash_sale_payload';
 
 @Injectable()
-export class FlashSaleService implements FlashSaleServiceInterface {
+export class FlashSaleService {
   constructor(private readonly flashSaleRepository: FlashSaleRepository) {}
 
   async create(dto: CreateFlashSalePayload) {
